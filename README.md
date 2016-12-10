ca44ebd78d1cc56fe18f4206d1a2566944115c63
jinyule <gkimfdqqqq@gmail.com>
2016-12-11 00:57:34 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,2 @@
+#A*算法的Java实现
+转载自http://software-talk.org/blog/2012/01/a-star-java/
---
+++ src/main/java/pathfinding/package-info.java
@@ -0,0 +1,24 @@
+/*    
+    Copyright (C) 2012 http://software-talk.org/ (developer@software-talk.org)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * This package may be used for its A* pathfinding algorithm.
+ * Extend the AbstractNode class as you need, but also implement the gethCosts
+ * method with a heuristic.
+ * The Map class may be extended as well.
+ */
+package pathfinding;
